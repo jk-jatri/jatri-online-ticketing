7e1082d13b6df3243f0d9d46e286a6e784570179
Joyonto <joyonto.jatri@gmail.com>
2021-06-10 16:50:00 +0600

    filter bus section

--- js/app.js
+++ js/app.js
@@ -5,3 +5,20 @@ $(function () {
         $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
     });
 });
+
+
+
+// select in 
+
+var expanded = false;
+
+function showCheckboxes() {
+  var checkboxes = document.getElementById("checkboxes");
+  if (!expanded) {
+    checkboxes.style.display = "block";
+    expanded = true;
+  } else {
+    checkboxes.style.display = "none";
+    expanded = false;
+  }
+}
